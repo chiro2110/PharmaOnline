023ac4b61a1265e1026e44a49d74d3a04f3895e3
chiro2110 <ro.ort211089@gmail.com>
2025-05-29 19:37:34 -0600

    Parte 11 completada

--- CursoMVC/CapaPresentacionAdmin/Controllers/HomeController.cs
+++ CursoMVC/CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace CapaPresentacionAdmin.Controllers
         {
             return View();
         }
-
+        [HttpGet]
         public JsonResult ListarUsuarios()
         {
             List<Usuario> oLista = new List<Usuario>();
@@ -29,6 +29,25 @@ namespace CapaPresentacionAdmin.Controllers
 
             return Json(new {data = oLista}, JsonRequestBehavior.AllowGet);
         }
+
+        [HttpPost]
+        public JsonResult GuardarUsuario(Usuario objeto)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+
+            if (objeto.IdUsuario == 0)
+            {
+                resultado = new CN_Usuarios().Registrar(objeto, out mensaje);
+            }
+            else
+            {
+                resultado = new CN_Usuarios().Editar(objeto, out mensaje);
+            }
+
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
 
     }
 } 
