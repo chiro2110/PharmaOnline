203e04d23a3b65906dc598c357b4654300748f1c
chiro2110 <ro.ort211089@gmail.com>
2025-06-03 00:29:26 -0600

    Creacion del modulo Cateogrias parte 16.5

---
+++ CursoMVC/CapaPresentacionAdmin/Controllers/MantenedorController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CapaEntidad;
+using CapaNegocio;
+
+namespace CapaPresentacionAdmin.Views.Home
+{
+    public class MantenedorController : Controller
+    {
+        // GET: Mantenedor
+        public ActionResult Categoria()
+        {
+            return View();
+        }
+        public ActionResult Marca()
+        {
+            return View();
+        }
+        public ActionResult Producto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarCategorias()
+        {
+            List<Categoria> oLista = new List<Categoria>();
+            oLista = new CN_Categoria().Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarCategoria(Categoria objeto)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+
+            if (objeto.IdCategoria == 0)
+            {
+                resultado = new CN_Categoria().Registrar(objeto, out mensaje);
+            }
+            else
+            {
+                resultado = new CN_Categoria().Editar(objeto, out mensaje);
+            }
+
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult EliminarCategoria(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new CN_Categoria().Eliminar(id, out mensaje);
+
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
