77eb2aa4e3647ba8af485f02efa8c9d00fb72848
chiro2110 <ro.ort211089@gmail.com>
2025-06-07 00:58:15 -0600

    Parte 25 completada logica de fechas en panel de control

--- CursoMVC/CapaPresentacionAdmin/App_Start/BundleConfig.cs
+++ CursoMVC/CapaPresentacionAdmin/App_Start/BundleConfig.cs
@@ -18,7 +18,7 @@ namespace CapaPresentacionAdmin
                         "~/Scripts/loadingoverlay/loadingoverlay.min.js",
                         "~/Scripts/sweetalert.min.js",
                         "~/Scripts/jquery.validate.js",
-                        "~/Scripts/jquery-ui.js",
+                        "~/Scripts/jquery-ui-1-14.1.js",
                         "~/Scripts/scripts.js"));
 
             //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
